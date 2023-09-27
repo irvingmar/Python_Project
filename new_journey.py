abdd225d66b69902f80ee428e8008adecc1c9263
irvingmar <145507109+irvingmar@users.noreply.github.com>
2023-09-27 13:24:46 -0500

    Add files via upload
    Collaborated with a team to develop an immersive text-based adventure game, leveraging Python's capabilities to facilitate dynamic storytelling with choices impacting narrative direction, character customization, a functioning inventory system, and randomized elements to enhance gameplay excitement and replay ability (2022).

---
+++ new_journey.py
@@ -0,0 +1,8 @@
+
+def Text():
+ print("Hello Demon Slayer, you have started a new journey, in this journey you will face total of 4 enemies.")
+ print("Each enemy will reduce your health by 250 points if the fight has been lost, you will only have one chance to make it.")
+ print("If you lost to all 4 enemies, your journey is over and you will not be missed. The world definitely have seen better warriors.")
+ print("On the other side, if you have managed to not die, you will join the Hall of Slayers.")
+
+ yes_no = input("Would you like to play?: ")
